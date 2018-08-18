f259497fa9439285f8908d67c96dd30c42419735
ejaramillo <ejaramillobarrera@gmail.com>
2018-08-17 17:48:31 -0700

    modificaciones al análisis

--- script_kaggle_titanic.R
+++ script_kaggle_titanic.R
@@ -22,16 +22,7 @@ rcd_train <- train %>%
 
 # Hay que ver cuales son las variables que tienen más valores perdidos
 
-plot_Missing <- function(data_in, title = NULL){
-  temp_df <- as.data.frame(ifelse(is.na(data_in), 0, 1))
-  temp_df <- temp_df[,order(colSums(temp_df))]  
-  data_temp <- expand.grid(list(x = 1:nrow(temp_df), y = colnames(temp_df)))
-  data_temp$m <- as.vector(as.matrix(temp_df))
-  data_temp <- data.frame(x = unlist(data_temp$x), y = unlist(data_temp$y), m = unlist(data_temp$m))
-  ggplot(data_temp) + geom_tile(aes(x=x, y=y, fill=factor(m))) + 
-    scale_fill_manual(values=c("white", "black"), name="Missing\n(0=Yes, 1=No)") +
-    theme_light() + ylab("") + xlab("") + ggtitle(title)
-}
+per
 
 
 
